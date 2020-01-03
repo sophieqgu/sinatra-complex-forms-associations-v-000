1de9690f919ef97454665812a615378b16480e1c
Sophie Gu <sophieqgu@gmail.com>
2020-01-03 17:06:51 +0000

    Automatically backed up by Learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -27,6 +27,7 @@ class PetsController < ApplicationController
   get '/pets/:id/edit' do 
     @pet = Pet.find(params[:id])
     erb :'/pets/edit'
+  end 
   
   patch '/pets/:id' do 
     @pet = Pet.find(params[:id])
