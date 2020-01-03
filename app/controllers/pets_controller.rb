1a55c01d7daa1a55dd736054b4be152dbfacacdb
Sophie Gu <sophieqgu@gmail.com>
2020-01-03 16:01:14 +0000

    Automatically backed up by Learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -27,7 +27,7 @@ class PetsController < ApplicationController
     @pet = Pet.find(params[:id])
     @pet.update(params[:pet])
     if !params["owner"]["name"].empty?
-      @owner.pets << Pet.create(name: params["pet"]["name"])
+      @pet.owner = Owner.create(name: params["owner"]["name"])
     end
     redirect to "pets/#{@pet.id}"
   end
