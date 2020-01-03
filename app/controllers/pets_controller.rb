8ee833b845e4a142aeb1073fe047d7ee1911dace
Sophie Gu <sophieqgu@gmail.com>
2020-01-03 17:12:48 +0000

    Automatically backed up by Learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -12,10 +12,13 @@ class PetsController < ApplicationController
 
   post '/pets' do 
     @pet = Pet.create(params[:pet])
+    
     if !params["owner"]["name"].empty?
       @pet.owner = Owner.create(name: params["owner"]["name"])
     end
+    
     @pet.save
+    
     redirect to "pets/#{@pet.id}"
   end
 
@@ -33,10 +36,13 @@ class PetsController < ApplicationController
   patch '/pets/:id' do 
     @pet = Pet.find(params[:id])
     @pet.update(params[:pet])
+    
     if !params["owner"]["name"].empty?
       @pet.owner = Owner.create(name: params["owner"]["name"])
     end
+    
     @pet.save
+    
     redirect to "pets/#{@pet.id}"
   end
 end
